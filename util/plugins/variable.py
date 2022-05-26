d035e2c4594dea3a304f62395465fa831611d5ab
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2022-05-26 13:17:17 +0200

    Update variable.py

--- util/plugins/variable.py
+++ util/plugins/variable.py
@@ -3,7 +3,7 @@ from colorama import Fore
 name = "ConsoleCord"
 author = "PauLem79"
 releaseurl = "https://github.com/Paulem79/ConsoleCord"
-THIS_VERSION = "0.1.18"
+THIS_VERSION = "0.1.19"
 
 y = Fore.LIGHTYELLOW_EX
 b = Fore.LIGHTBLUE_EX

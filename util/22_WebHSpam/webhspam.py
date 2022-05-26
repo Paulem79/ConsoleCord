240f72832e2b091f114f14e19f29a39949486f2b
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2022-05-26 17:46:07 +0200

    Update v0.1.23

--- util/22_WebHSpam/webhspam.py
+++ util/22_WebHSpam/webhspam.py
@@ -7,6 +7,7 @@ from util.plugins.commun import *
 from cc import main
 
 def webhookspam():
+    antiratelimit = 0
     setTitle("WebHook Spammer")
     clear()
     webhspamtitle()
@@ -16,8 +17,16 @@ def webhookspam():
     message = input(f"""{y}[{b}#{y}]{w} Message: """)
     print(f"""\n{y}[{w}+{y}]{w} Amount of time for the attack (s) """)
     timer = input(f"""{y}[{b}#{y}]{w} Amount: """)
+    print(f"""\n{y}[{w}+{y}]{w} Anti Rate-Limit ? (Y/N)""")
+    ratelimit = input(f"""{y}[{b}#{y}]{w} Response : """)
+    print(f"""{y}[{w}+{y}]{w} {r}ALPHA{w} Create Webhhok to bypass Rate-Limit ? (Y/N)""")
+    bypassratelimit = input(f"""{y}[{b}#{y}]{w} Bypass ? : """)
+    if bypassratelimit == "Y" or "y" or "yes":
+        print(f"""{y}[{w}+{y}]{w} {r}ALPHA{w} Put a channel ID to activate the system : """)
+        chidwh = input(f"""{y}[{b}#{y}]{w} Channel ID : """)
     input(f"""\n\n{y}[{b}#{y}]{w} Press ENTER to Valid""")
 
+    #message.channel.create_webhook(name="mywebhook")
     try:
         timeout = time.time() + 1 * float(timer)
 
@@ -27,15 +36,20 @@ def webhookspam():
                 json = {"content" : message},
                 params = {'wait' : True}
             )
-            clear()
-            time.sleep(1)
-            if response.status_code == 204 or response.status_code == 200:
-                print(f"""{y}[{Fore.LIGHTGREEN_EX }!{y}]{w} Message sent""")
-            elif response.status_code == 429:
-                print(f"""{y}[{Fore.LIGHTRED_EX }!{y}]{w} Rate limited ({response.json()['retry_after']}ms)""")
-                time.sleep(response.json()["retry_after"] / 1000)
+            if antiratelimit != 30:
+                if response.status_code == 204 or response.status_code == 200:
+                    print(f"""{y}[{Fore.LIGHTGREEN_EX }!{y}]{w} Message sent""")
+                    if ratelimit == "y" or "Y" or "yes":
+                        antiratelimit += 1
+                elif response.status_code == 429:
+                    print(f"""{y}[{Fore.LIGHTRED_EX }!{y}]{w} Rate limited ({response.json()['retry_after']}ms)""")
+                    time.sleep(response.json()["retry_after"] / 1000)
+                else:
+                    print(f"""{y}[{Fore.LIGHTRED_EX }!{y}]{w} Error code: {response.status_code}""")
             else:
-                print(f"""{y}[{Fore.LIGHTRED_EX }!{y}]{w} Error code: {response.status_code}""")
+                print("Anti Rate-Limit, pause for 15 seconds")
+                time.sleep(15)
+                antiratelimit = 0
     except:
         print(f"""      {y}[{Fore.LIGHTRED_EX }!{y}]{w} Your request is invalid !""")
         time.sleep(2)
---
+++ variable.py
@@ -0,0 +1,12 @@
+from colorama import Fore
+
+name = "ConsoleCord"
+author = "PauLem79"
+releaseurl = "https://github.com/Paulem79/ConsoleCord"
+THIS_VERSION = "0.1.23"
+
+y = Fore.YELLOW
+b = Fore.LIGHTBLUE_EX
+w = Fore.LIGHTWHITE_EX
+r = Fore.RED
+g = Fore.GREEN
